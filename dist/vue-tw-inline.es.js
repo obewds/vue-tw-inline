9e0fc49f11ca78b3423dfe2bd643d1f9ae8de9b5
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-05-30 11:39:00 -0400

    0.4.0: bump minor - updated deps w/ vue-tw-el now v2.0

--- dist/vue-tw-inline.es.js
+++ dist/vue-tw-inline.es.js
@@ -2,6 +2,7 @@ import { defineComponent, openBlock, createBlock, unref, withCtx, renderSlot } f
 import { VueTwEl } from "@obewds/vue-tw-el";
 import { inlineLevelElementTags } from "@obewds/vue-validators";
 const _sfc_main = /* @__PURE__ */ defineComponent({
+  name: "VueTwInline",
   props: {
     bgPalette: {
       type: String,
